05e5ef4ad2b81d4a74e87beeb147ec1cb3efb660
Myra Smith <myra.fowlkes.smith@gmail.com>
2021-07-20 09:48:56 -0400

    changed api call

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,13 +6,13 @@ const API = apiURL();
 function App() {
   const [days, setDays] = useState([]);
   useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => setDays(response.data),
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
+    // axios
+    //   .get(`${API}/test`)
+    //   .then(
+    //     (response) => setDays(response.data),
+    //     (error) => console.log("get", error)
+    //   )
+    //   .catch((c) => console.warn("catch", c));
   }, []);
   return (
     <div>
